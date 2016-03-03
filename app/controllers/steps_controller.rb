9a4796e2fec96c0cd71737a47a21e2819c01cd89
Kate Studwell <katestudwell@gmail.com>
2016-03-03 17:30:39 -0500

    Set up intial steps controller actions, routes, and tests.

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,6 +1,47 @@
 class StepsController < ApplicationController
-  def show
+  before_action :set_step, only: [:show, :destroy]
 
+  def index
+    @steps = Step.all
   end
+
+  def show
+  end
+
+  def new
+    @step = Step.new
+  end
+
+
+  def create
+    @step = Step.new(step_params)
+
+    respond_to do |format|
+      if @step.save
+        format.html { redirect_to @step, notice: 'Step was successfully created.' }
+        format.json { render :show, status: :created, location: @step }
+      else
+        format.html { render :new }
+        format.json { render json: @step.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @step.destroy
+    respond_to do |format|
+      format.html { redirect_to steps_url, notice: 'Step was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_step
+      @step = Step.find(params[:id])
+    end
+
+    def step_params
+      params.require(:step).permit(:date_id, :step_count, :created_at, :updated_at)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
 
   resources :exercises
   resources :meals
+  resources :steps
 
   get 'weights' => 'weights#show'
-  get 'steps' => 'steps#show'
 
   get 'dashboard/show'
   root 'dashboard#show'
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

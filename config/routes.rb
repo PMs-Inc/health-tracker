9bdc2c31b1534d54e6f1a530faaa1590e61de0af
Kate Studwell <katestudwell@gmail.com>
2016-03-03 15:19:54 -0500

    Create initial dashboard controller and model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'dashboard/show'
+
   resources :exercises
-  root 'hello#show'
+  root 'dashboard#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

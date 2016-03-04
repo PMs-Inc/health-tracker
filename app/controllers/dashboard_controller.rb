46a31a2402d26493d56b68f7cfdc999d0639cef7
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-03-04 13:14:54 -0500

    Add empty state to all list partials

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,18 @@
 class DashboardController < ApplicationController
   def show
-    @dashboard = Dashboard.new(params[:date].to_i)
+
+    date_int = params[:date].to_i
+
+    @dashboard = Dashboard.new(date_int)
     @exercise = Exercise.new
     @options = ExerciseType.get_options
     @meal = Meal.new
     @weight = Weight.new
     @step = Step.new
 
-    @exercises = Exercise.where(date_id: params[:date].to_i)
-    @meals = Meal.where(date_id: params[:date].to_i)
-    @weights = Weight.where(date_id: params[:date].to_i)
-    @steps = Step.where(date_id: params[:date].to_i)
+    @exercises = Exercise.where(date_id: date_int)
+    @meals = Meal.where(date_id: date_int)
+    @weights = Weight.where(date_id: date_int)
+    @steps = Step.where(date_id: date_int)
   end
 end

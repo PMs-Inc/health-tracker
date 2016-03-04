dc7564b9ce76264a7b454b2433a861bfd4e490d2
Kate Studwell <katestudwell@gmail.com>
2016-03-03 21:43:03 -0500

    Create intial unit tests for exercise and step models.

--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ExerciseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "can get duration" do
+    exercise = Exercise.create!(duration: 15)
+    assert_equal 15, exercise.duration
+  end
+
+  test "can get exercise type id" do
+    #update this to pull the exercise name when that model has been created
+    exercise = Exercise.create!(exercise_type_id: 1)
+    assert_equal 1, exercise.exercise_type_id
+  end
+
+
+
+
 end

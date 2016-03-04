8051ca53c9de49ffa0f3341c65d3725938fd30d1
Kate Studwell <katestudwell@gmail.com>
2016-03-04 13:26:52 -0500

    Add tests to dashboard controller.

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -6,4 +6,29 @@ class DashboardControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get proper weight for day" do
+    get :show, date: weights(:one).date_id
+    assert_response :success
+    assert response.body.include?("175")
+  end
+
+  test "should get proper meals for day" do
+    get :show, date: meals(:three).date_id
+    assert_response :success
+    assert response.body.include?("Pizza Slice")
+  end
+
+  test "should get proper exercises for day" do
+    get :show, date: exercises(:five).date_id
+    assert_response :success
+    assert response.body.include?("10")
+  end
+
+  test "should get proper steps for day" do
+    get :show, date: steps(:two).date_id
+    assert_response :success
+    assert response.body.include?("7567")
+  end
+
+
 end

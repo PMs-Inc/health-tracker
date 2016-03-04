5e4e917330b77df3876bb861e1408d1b545f7944
Kate Studwell <katestudwell@gmail.com>
2016-03-03 22:07:45 -0500

    Create current weight method.

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,3 +1,13 @@
 class Dashboard
+  def initialize
+    @exercises = Exercise.all
+    @meals = Meal.all
+    @steps = Step.all
+    @weight = Weight.all
+  end
 
-end  
+  def current_weight
+    Weight.order(:date_id).last.weight
+  end
+
+end
